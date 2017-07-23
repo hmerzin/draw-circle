0a2bd86016376e134a06dc04bda1ff7f9274d24b
Harry Merzin <harry@milosets.me>
2017-07-23 10:56:09 -0400

    push to heroku

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Alexa = require("alexa-app");
+
+const expressApp = express();
+
+const alexa = alexa.app("draw");
+
+alexa.express({ expressApp: expressApp });
+
+alexa.intent(
+  "DrawIntent",
+  {
+    dialog: {
+      type: "delegate"
+    },
+
+    slots: {
+      size: "Size",
+      color: "AMAZON.Color"
+    }
+  },
+  (req, res) => {
+    res.say("check your browser");
+  }
+);
